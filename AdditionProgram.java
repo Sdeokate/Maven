91ad121833443ead5eb9756da624190658bb3769
shubham <su.deokate@gmail.com>
2019-11-24 12:37:41 +0530

    addition

---
+++ AdditionProgram.java
@@ -0,0 +1,26 @@
+package addition;
+
+public class AdditionProgram {
+	
+	
+	
+	 
+	 public static int addValues()
+	 {
+		 int number1=5,number2=4;
+			
+		 int result = number1+number2;
+		 System.out.println(" Addition Is :" +result);
+		 return(result);
+		 
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result = addValues();
+		 
+		
+	}
+
+}
